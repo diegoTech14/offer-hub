b7d6ef52196d0987dec151d2eaee84752a9aad7d
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2026-01-26 06:20:16 +0100

    removed typos and unnecessary comments

--- backend/src/routes/task.route.ts
+++ backend/src/routes/task.route.ts
@@ -1,8 +1,8 @@
-import { getTaskRecordsByClientHandler } from "@/controllers/task.controller";
+import { getClientTaskRecords } from "@/controllers/task.controller";
 import { Router } from "express";
 
 const router = Router();
 
-router.get("/client/:clientId", getTaskRecordsByClientHandler); // Controller to get tasks records by client id
+router.get("/client/:clientId", getClientTaskRecords); // Controller to get tasks records by client id
 
 export default router;
